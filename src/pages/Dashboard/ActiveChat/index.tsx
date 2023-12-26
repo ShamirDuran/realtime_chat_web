ecd55aa8e493e12d50c205a95dad8b66d835c5fd
Shamir Duran <shamirduran15@gmail.com>
2023-12-26 17:16:55 -0500

    Auto scroll to end messages

--- src/pages/Dashboard/ActiveChat/index.tsx
+++ src/pages/Dashboard/ActiveChat/index.tsx
@@ -3,27 +3,35 @@ import { useStyles } from '../../../hooks'
 import { Header } from './Header'
 import { Message } from './Message'
 import fakeChat from './data'
-import React from 'react'
+import React, { useRef } from 'react'
 import { InputMessage } from './InputMessage'
 
 export const ActiveChat = () => {
   const styles = useStyles()
+  const scrollRef: React.RefObject<HTMLDivElement> = useRef(null)
   const [messages, setMessages] = React.useState<any>([])
 
   React.useEffect(() => {
     setMessages(fakeChat(30))
+
+    // weird trick to scroll to bottom on first render
+    const scrollContainer = scrollRef.current
+    if (scrollContainer) {
+      setTimeout(() => {
+        scrollContainer.scrollTop = scrollContainer.scrollHeight
+      }, 100)
+    }
   }, [])
 
   return (
     <Stack sx={{ flexGrow: 1, maxHeight: '100vh' }} position='relative'>
       <Header />
 
-      <Box bgcolor='background.paper' pb={10} overflow='auto'>
+      <Box ref={scrollRef} bgcolor='background.paper' pb={10} overflow='auto'>
         <Stack
-          direction={'column-reverse'}
           flexGrow={1}
-          overflow='auto'
           spacing={1.5}
+          direction={'column-reverse'}
           sx={{
             py: styles.margin.root.vertical,
             px: styles.margin.root.horizontal,
